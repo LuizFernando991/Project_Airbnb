658c401b32cfb13e3609f7a38dcd52804ac7097c
unknown <lfernando.r991@gmail.com>
2023-06-07 15:33:21 -0300

    actions changed folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/navbar/Navbar'
 import RegisterModal from '@/components/modals/RegisterModal'
 import LoginModal from '@/components/modals/LoginModal'
 import ToasterProvider from '@/providers/ToasterProvider'
-import getCurrentUser from '@/app/actions/getCurrentUser'
+import getCurrentUser from '@/actions/getCurrentUser'
 
 const font = Nunito({
   subsets: ['latin']
